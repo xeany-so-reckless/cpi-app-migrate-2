54a44d0c3b1bed2ee75ec3a95414a19e4a053cac
Umamm. <ahmadkhotibul321@gmail.com>
2026-09-22 09:01:02 +0700

    dashboard controller edit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -91,7 +91,13 @@ class DashboardController extends Controller
     "icon" => "inventory_2",
     "info" => "Sistem pengelolaan stok dan aktivitas keluar masuk barang",
     "url"  => "http://10.60.22.31/warehouse",
-]
+],
+            [
+    "name" => "WEB BACKUP",
+    "icon" => "history",
+    "info" => "Akses ke sistem web versi sebelumnya untuk kebutuhan backup dan referensi",
+    "url"  => "10.60.22.31/web/",
+],
 
         ];
 
